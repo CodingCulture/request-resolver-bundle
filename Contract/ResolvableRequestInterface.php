65d59fb003857555aee5bfa0a0caed3c54619fb9
Niels <nielsvermaut@gmail.com>
2018-01-24 22:50:22 +0100

    Update ResolvableRequestInterface.php

--- Contract/ResolvableRequestInterface.php
+++ Contract/ResolvableRequestInterface.php
@@ -11,9 +11,29 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
  */
 interface ResolvableRequestInterface
 {
+    /**
+     * Should configure the passed OptionsResolver to match the expectations in structure and type of the
+     * request.
+     *
+     * @param OptionsResolver $resolver
+     *
+     * @return OptionsResolver
+     */
     public function defineOptions(OptionsResolver $resolver): OptionsResolver;
 
+    /**
+     * Should store the validated request options for use.
+     *
+     * @param array $options
+     *
+     * @return void
+     */
     public function setOptions(array $options): void;
 
+    /**
+     * Should define the Content-Type header the request should have.
+     *
+     * @return string
+     */
     public function getContentType(): string;
 }
