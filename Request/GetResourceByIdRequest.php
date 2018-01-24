c5a7c82228d455f291122814e6111d702353f4ac
Niels <nielsvermaut@gmail.com>
2018-01-24 22:45:34 +0100

    Standardise method signature in comment.

--- Request/GetResourceByIdRequest.php
+++ Request/GetResourceByIdRequest.php
@@ -12,11 +12,15 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
  *
  * This is a quite generic request, but also highlights the usage and requirements of this request resolver bundle.
  * Once you have implemented all required methods from the ResolvableRequestInterface, you can then add the shorthand methods
- * for fetching properties of the request, shown as "getId".
+ * for fetching properties of the request, shown as ResolvableRequestInterface::getId.
  *
- * Using an getOptions() method should be considered faux pas, in that the aim of this bundle is to structure the request.
+ * Using an ResolvableRequestInterface::getOptions() method should be considered faux pas, in that the aim of this bundle is to structure the request.
  * If you want to do more in depth assertions -- like checking the database for a record matching the id -- you should look
  * at implementing it on the service level. Request objects are meant purely for structure and type.
+ *
+ * ResolvableRequestInterface::setOptions() should always wire up the private $options.
+ *
+ * ResolvableRequestInterface::getContentType() should return which Content-Type is expected of the request.
  */
 class GetResourceByIdRequest implements ResolvableRequestInterface
 {
