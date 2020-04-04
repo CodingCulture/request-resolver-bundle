35aa64d2209214a51fd94bd7cf36307f19bfb1cf
Niels Vermaut <nielsvermaut@gmail.com>
2020-04-04 13:33:16 +0200

    Backwards compat doesn't work, just bumping it

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,17 +18,6 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        try {
-            $treeBuilder = new TreeBuilder();
-            $rootNode = $treeBuilder->root('codingculture_requestresolver');
-        } catch (Exception $exception) {
-            $treeBuilder = new TreeBuilder('codingculture_requestresolver');
-        }
-
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
-
-        return $treeBuilder;
+        return new TreeBuilder('codingculture_requestresolver');
     }
 }
