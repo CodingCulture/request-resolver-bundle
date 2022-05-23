e84992e5b85947829039e62e8ab9fbc8ec33fc29
Nikolaj Malezsewski <nikolaj.mal@gmail.com>
2022-05-23 18:05:28 +0200

    no unnecessary white line pls

--- Resolver/RequestResolver.php
+++ Resolver/RequestResolver.php
@@ -2,7 +2,6 @@
 
 namespace CodingCulture\RequestResolverBundle\Resolver;
 
-
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RequestStack;
